f8cfa8ee5aba20f817895e14147fdf856a8091c3
Komal Rathi <komalrathi068@gmail.com>
2019-10-11 20:27:07 +0100

    changes to css

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 //alert ("Hello and welcome to my webpage! Scroll down to find out more about me.");
 
 console.log ("");
+
+var x="hello";
+welcomeMsg(x);
+x="goodbye";
+welcomeMsg(x); 
+
+//function welcomeMsg (msg){
+	//alert(msg);}
+
+
